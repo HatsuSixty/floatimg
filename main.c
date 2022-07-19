5eab4c8ea655549641e9ad384e03edc50b7a2715
robertosixty1 <roberto011herminegildo@gmail.com>
2022-07-19 11:27:06 -0300

    Add usage and flags

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#include <assert.h>
+#include <string.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -14,8 +15,31 @@ void scc(int code) {
     }
 }
 
+void usage(FILE* stream, const char* program_name) {
+    fprintf(stream, "Usage: %s [FLAGS] <IMAGE>\n", program_name);
+    fprintf(stream, "    FLAGS:\n");
+    fprintf(stream, "        --help | -h          Show this help and exit with 0 code\n");
+    fprintf(stream, "    IMAGE:\n");
+    fprintf(stream, "        That is the image that is going to be displayed\n");
+}
+
 int main(int argc, const char** argv) {
-    assert((argc > 1) && "Image not provided");
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: Not enough arguments were provided\n");
+        usage(stderr, argv[0]);
+        exit(1);
+    }
+
+    const char* file;
+
+    for (int i = 1; i < argc; ++i) {
+        if ((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "--h") == 0)) {
+            usage(stdout, argv[0]);
+            exit(0);
+        } else {
+            file = argv[i];
+        }
+    }
 
     scc(SDL_Init(SDL_INIT_EVERYTHING));
     scc(IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF | IMG_INIT_WEBP));
@@ -24,7 +48,7 @@ int main(int argc, const char** argv) {
     int height;
     {
         int n;
-        unsigned char *image_data = stbi_load(argv[1], &width, &height, &n, 0);
+        unsigned char *image_data = stbi_load(file, &width, &height, &n, 0);
         stbi_image_free(image_data);
         if (image_data == NULL) {
             fprintf(stderr, "ERROR: Invalid image\n");
@@ -32,14 +56,14 @@ int main(int argc, const char** argv) {
         }
     }
 
-    SDL_Window* window = SDL_CreateShapedWindow(argv[1],
+    SDL_Window* window = SDL_CreateShapedWindow(file,
                                                 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                                width, height, SDL_WINDOW_BORDERLESS);
+                                                width, height, 0);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 
-    SDL_Surface* image = IMG_Load(argv[1]);
+    SDL_Surface* image = IMG_Load(file);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
     SDL_SetWindowShape(window, image, 0);
 
